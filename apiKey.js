185d897330a70a6402e5319b3d24e73a789c447d
Daniel.Yu <adam741963@gmail.com>
2020-07-27 20:22:29 -0700

    fetch function

--- apiKey.js
+++ apiKey.js
@@ -1,2 +1,10 @@
 const openWeatherApiKey = "e93223a6b1823d41860077c8e54b5206"; // D
-const googleMapApiKey = "AIzaSyDS9OzwklRtVCcnbDPST9OLhGkh9dQj-xA"; // D
+const googleMapApiKey = "AIzaSyDmZhf4Cy3XVS_6hruDDGNfWfd0Uaxfxp4"; // D
+
+const requestCity = async (cityName) => {
+    const WeatherURL = "https://api.openweathermap.org/data/2.5/weather";
+    const query = `?q=${cityName}&appid=${googleMapApiKey}`;
+    const response = await fetch((WeatherURL + query), {method: "GET"});
+    const data = await response.json();
+    return data;
+}

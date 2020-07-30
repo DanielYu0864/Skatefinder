b0cc1806121f353a9fd162544be6648c8074b6fa
Daniel.Yu <adam741963@gmail.com>
2020-07-29 18:51:20 -0700

    forecast and uvi

--- apiKey.js
+++ apiKey.js
@@ -1,10 +1,30 @@
 const openWeatherApiKey = "e93223a6b1823d41860077c8e54b5206"; // D
 const googleMapApiKey = "AIzaSyDmZhf4Cy3XVS_6hruDDGNfWfd0Uaxfxp4"; // D
 
-const requestCity = async (cityName) => {
+const requestCityWeather = async (cityName) => {
     const WeatherURL = "https://api.openweathermap.org/data/2.5/weather";
     const query = `?q=${cityName}&appid=${openWeatherApiKey}`;
     const response = await fetch((WeatherURL + query), {method: "GET"});
+    const data = await response.json();
+    return data;
+}
+
+const requestCityUVI = async (cityName) => {
+    const uviAPIURL = "https://api.openweathermap.org/data/2.5/uvi";
+    let lat = cityName.coord.lat;
+    let lon = cityName.coord.lon;
+    const query = `?lat=${lat}&lon=${lon}&appid=${openWeatherApiKey}`;
+    const response = await fetch((uviAPIURL + query), {method: "GET"});
+    const data = await response.json();
+    return data;
+}
+
+const requestCityForecast = async (cityid) => {
+    const queryforecastURL = "https://api.openweathermap.org/data/2.5/forecast";
+    const query = `?id=${cityid}&appid=${openWeatherApiKey}`;
+    // make fetch call (promise call)
+    const response = await fetch((queryforecastURL + query), {method: "GET"});
+    // promise data
     const data = await response.json();
     return data;
 }
--- regular.js
+++ regular.js
@@ -8,7 +8,7 @@ function searchLocation(event) {
     const citySearched = searchLocationInput.value.trim();
     console.log(citySearched);
 
-    requestCity(citySearched).then((data) => {
+    requestCityWeather(citySearched).then((data) => {
         updataWeather(data);
     }).catch((error) => {
         console.log(error);
@@ -22,7 +22,52 @@ function updataWeather(city) {
     $(".condition").html(city.weather[0].description);
     $(".temp").html(((city.main.temp - 273.15) * 1.80 + 32).toFixed(0));
 
+    requestCityUVI(city).then((data) => {
+        uvIndex(data);
+    }).catch((error) => {
+        console.log(error);
+    })
 
+    requestCityForecast(city.id).then((data) => {
+        forecastWeather(data);
+    }).catch((error) => {
+        console.log(error);
+    })
+
+}
+
+function forecastWeather(response) {
+    console.log(response);
+    for (let i=0;i<5;i++){
+        let date = new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+        let tempK = response.list[((i+1)*8)-1].main.temp;
+        let tempF = ((tempK - 273.15) * 1.80 + 32).toFixed(0);
+        let condition = response.list[((i+1)*8)-1].weather[0].description;
+        let humidity = response.list[((i+1)*8)-1].main.humidity;
+
+        $("#fDate"+i).html(date);
+        $("#fTemp"+i).html(tempF);
+        $("#fCondition"+i).html(condition);
+        $("#fHumidity"+i).html(` ${humidity}%`);
+    }
+}
+
+function uvIndex(response) {
+    const uviResults = response;
+    const uvi = uviResults.value;
+    $(".uv-index").html(uvi);
+
+    if (uvi < 3) {
+        $(".uv-index").css("background-color", "green");
+      } else if (uvi < 6) {
+        $(".uv-index").css("background-color", "yellow");
+      } else if (uvi < 8) {
+        $(".uv-index").css("background-color", "orange");
+      } else if (uvi < 11) {
+        $(".uv-index").css("background-color", "red");
+      } else {
+        $(".uv-index").css("background-color", "purple");
+      }
 }
 
 searchButton.addEventListener("click", searchLocation);

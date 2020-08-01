f230d732e421dbf3a44246bcb0ca3e34c2f1a5f5
Daniel.Yu <adam741963@gmail.com>
2020-08-01 13:41:33 -0700

    map api

--- maps.js
+++ maps.js
@@ -1,12 +1,12 @@
-function initMap(city) {
-    let longitude = city.coord.lon;
-    let latitude = city.coord.lat;
-    var pos = {lat: latitude, lng: longitude};
-    var opt = {
-        center: pos,
-        zoom: 12,
-    };
-    var myMap = new google.maps.Map(document.getElementById("map-display"), opt);
+// function initMap(city) {
+//     let longitude = city.coord.lon;
+//     let latitude = city.coord.lat;
+//     var pos = {lat: latitude, lng: longitude};
+//     var opt = {
+//         center: pos,
+//         zoom: 12,
+//     };
+//     var myMap = new google.maps.Map(document.getElementById("map-display"), opt);
 
-    console.log(myMap);
-}
+//     console.log(myMap);
+// }
